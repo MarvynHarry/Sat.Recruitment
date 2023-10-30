ea218befde65a5ff9421d4fb207c83803ea720e3
Marvyn-Harryson <marvynharry@gmail.com>
2023-10-30 10:56:09 -0400

    Commit Unit Test

---
+++ Sat.Recruitment.Test/Services/UsersServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using Sat.Recruitment.Api.Interfaces;
+using Sat.Recruitment.Api.Models;
+using Sat.Recruitment.Api.Services;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sat.Recruitment.Test.Services
+{
+    public class UsersServiceTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<IValidationService<User>> mockValidationService;
+        private Mock<IConfiguration> mockConfiguration;
+
+        public UsersServiceTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockValidationService = this.mockRepository.Create<IValidationService<User>>();
+            this.mockConfiguration = this.mockRepository.Create<IConfiguration>();
+        }
+
+        private UsersService CreateService()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            IConfiguration config = builder.Build();
+            mockValidationService.SetupAllProperties();
+            return new UsersService(
+                this.mockValidationService.Object,
+                config);
+        }
+
+        [Fact]
+        public async Task CreateUser_StateUnderTest_ExpectedBehavior()
+        {
+
+            // Arrange
+            var service = this.CreateService();
+            User newUser = new()
+            {
+                Address = "Alvear y Colombres",
+                Email = "user+polo@example.com",
+                Money = 59,
+                Name = "Marvyn harryson Jeanty",
+                Phone = "+8496262888",
+                UserType = "Normal",
+            };
+
+            // Act
+            var result = await service.CreateUser(newUser);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+
+
+            // Act
+            result = await service.CreateUser(newUser);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+
+            this.mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public async Task Getusers_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var service = this.CreateService();
+
+            // Act
+            var result = await service.Getusers();
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
